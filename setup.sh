b625ac9d1b33d0e0ac892e2531819271780dff93
Nbkeatley <22612695+Nbkeatley@users.noreply.github.com>
2025-05-13 14:37:35 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,10 +4,11 @@ sudo apt update
 
 sudo apt install -y tesseract-ocr libtesseract-dev
 
-sudo apt install -y swig3.0
-pip install jamspell
+pip install --upgrade pip==22.3.1 setuptools==65.6.3 wheel==0.38.4
 
+pip install -r requirements.txt
+
+# Download Spellcheck English-language dictionary for JamSpell
+sudo apt install -y swig3.0
 wget https://github.com/bakwc/JamSpell-models/raw/master/en.tar.gz -O en.tar.gz
 tar -xvf en.tar.gz
-
-
